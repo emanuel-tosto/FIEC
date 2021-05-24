fe886b83cac243ca25e7f333507304c9a0d9a7bd
Emanuel Vasconcelos <emanueltosto@gmail.com>
2021-05-23 21:28:29 -0300

    Query Item B

--- B.py
+++ B.py
@@ -1,5 +1,5 @@
 SELECT 
-    IF(uf='CE','Ceara',IF(regiao_geografica='Nordeste','Nordeste','Brasil')) AS Localidade, 
+    IF(uf='CE','Ceara',IF(regiao_geografica='Nordeste','Nordeste','Brasil')) AS localidade, 
     
     IF(uf='CE',(SELECT count(*) FROM atracacao_fato WHERE uf = 'CE'),IF(regiao_geografica='Nordeste',(SELECT count(*) FROM atracacao_fato WHERE regiao_geografica='Nordeste'),(SELECT count(*) FROM atracacao_fato)) AS Numero_Atracacoes_Por_Localidade,
     
